3659952be58a62820cda8faf90c54c969547fdb2
Yohei <yohei.isshiki@rakuten.com>
2021-10-22 16:35:56 +0900

    vue 3 migration completed.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="edelRitter">
+    <uiLoader></uiLoader>
+    <uiLogoNav></uiLogoNav>
+    <Hero></Hero>
+    <Info></Info>
+    <Works></Works>
+    <Contact></Contact>
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
+<script>
+import uiLogoNav from './stories/components/ui_component_navigation_logo.vue'
+import uiLoader from './stories/components/ui_component_loader.vue'
+import Hero from './portfolio/landing.vue'
+import Info from './portfolio/information.vue'
+import Works from './portfolio/contents.vue'
+import Contact from './portfolio/contact.vue'
+export default {
+  name: 'App',
+  components: {
+    'uiLogoNav': uiLogoNav,
+    'uiLoader': uiLoader,
+    'Hero': Hero,
+    'Info': Info,
+    'Works': Works,
+    'Contact': Contact
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Oswald&family=Playfair+Display&display=swap');
+@font-face {
+    font-style: normal;
+    font-weight: 400;
+    font-display: auto;
+    src: url(../fonts/fa/fa-regular-400.eot);
+    src: url(../fonts/fa/fa-regular-400.eot?#iefix) format("embedded-opentype"), url(../fonts/fa/fa-regular-400.woff2) format("woff2"), url(../fonts/fa/fa-regular-400.woff) format("woff"), url(../fonts/fa/fa-regular-400.ttf) format("truetype"), url(../fonts/fa/fa-regular-400.svg#fontawesome) format("svg")
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
-import { createApp } from 'vue'
-import Vue from 'vue'
-import App from './App.vue'
-import AOS from 'aos'
-import VueParallaxJs from 'vue-parallax-js'
-
 import './stories/assets/scss/ui_all.scss';
 import './portfolio/assets/scss/_portfolio.scss';
-import router from './router'
 
-const portfolio = createApp(App).use(router);
+import { createApp } from "vue";
+import App from './App.vue'
+import AOS from 'aos'
+
 AOS.init();
-Vue.use(VueParallaxJs);
-
-portfolio.mount('#app')
+createApp(App).mount('#app')
